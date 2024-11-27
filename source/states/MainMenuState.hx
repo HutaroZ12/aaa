af43dced40e23d1bebb91275c641fcc2436db80c
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-11-27 09:57:27 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -135,12 +135,7 @@ class MainMenuState extends MusicBeatState
 
 		super.create();
 
-		FlxG.camera.follow(camFollow, null, 9);
-	}
-
-	var selectedSomethin:Bool = false;
-
-	override function update(elapsed:Float)
+		Ft)
 	{
 		if (FlxG.sound.music.volume < 0.8)
 		{
@@ -174,11 +169,11 @@ class MainMenuState extends MusicBeatState
 				else
 				{
 					selectedSomethin = true;
-
-					if (ClientPrefs.data.flashing)
-						FlxG.camera.flash(FlxColor.WHITE, 1);
+					FlxG.camera.flash(FlxColor.WHITE, 1);
 					
-						FlxTween.tween(menuItem, {x:-3200}, 2.2, {ease: FlxEase.expoInOut});
+					FlxTween.tween(menuItem, {x:-3200}, 2.2, {ease: FlxEase.expoInOut});
+					
+					//if (ClientPrefs.data.flashing)
 						//FlxFlicker.flicker(magenta, 1.1, 0.15, false);
 
 					FlxFlicker.flicker(menuItems.members[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
