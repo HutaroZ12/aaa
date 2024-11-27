c1c86412424faf4711f566c032fa18cf4c0fe39f
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-11-27 12:42:10 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -171,8 +171,8 @@ class MainMenuState extends MusicBeatState
 					selectedSomethin = true;
 					FlxG.camera.flash(FlxColor.WHITE, 1);
 					
-					//if (ClientPrefs.data.flashing)
-					//	FlxFlicker.flicker(magenta, 1.1, 0.15, false);
+					if (ClientPrefs.data.flashing)
+						FlxFlicker.flicker(magenta, 1.1, 0.15, false);
 
 					FlxFlicker.flicker(menuItems.members[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
 					{
