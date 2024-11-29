f4303deda7dc975136f7d0e28fbc2efede37e922
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-11-29 20:52:27 -0300

    Update MainMenuState.hx

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -75,8 +75,8 @@ class MainMenuState extends MusicBeatState
 			var menuItem:FlxSprite = new FlxSprite(0, (i * 140) + offset);
 			menuItem.antialiasing = ClientPrefs.data.antialiasing;
 			menuItem.x += 1200;
-			//menuItem.scale.x = 0.7;
-	                //menuItem.scale.y = 0.7;
+			menuItem.scale.x = 0.7;
+	                menuItem.scale.y = 0.7;
 			menuItem.frames = Paths.getSparrowAtlas('mainmenu/menu_' + optionShit[i]);
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " idle", 10);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " selected", 12);
