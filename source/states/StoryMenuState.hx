75df799e3dbe462193af65314e8d11e45fb75d1c
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-12-07 14:12:44 -0300

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -169,7 +169,7 @@ class StoryMenuState extends MusicBeatState
 			weekCharacterThing.x = 1200;
 			grpWeekCharacters.add(weekCharacterThing);
 
-			FlxTween.tween(weekCharacterThing, {x:0}, 2.4, {ease: FlxEase.expoInOut});
+			FlxTween.tween(weekCharacterThing, {x:20}, 2.5, {ease: FlxEase.expoInOut});
 		}
 
 		difficultySelectors = new FlxGroup();
@@ -209,6 +209,7 @@ class StoryMenuState extends MusicBeatState
 		add(grpWeekCharacters);
 		add(bgBar);
 		add(difficultySelectors);
+		add(grpWeekText);
 
 		var tracksSprite:FlxSprite = new FlxSprite(FlxG.width * 0.07 + 100, bgSprite.y + 425).loadGraphic(Paths.image('Menu_Tracks'));
 		tracksSprite.antialiasing = ClientPrefs.data.antialiasing;
