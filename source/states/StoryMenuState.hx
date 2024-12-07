6c9bc39ad0ac674bc8dc096050dccb6cfe0b1910
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-12-07 03:28:07 -0300

    Update StoryMenuState.hx

--- source/states/StoryMenuState.hx
+++ source/states/StoryMenuState.hx
@@ -173,7 +173,10 @@ class StoryMenuState extends MusicBeatState
 		{
 			var weekCharacterThing:MenuCharacter = new MenuCharacter((FlxG.width * 0.25) * (1 + char) - 150, charArray[char]);
 			weekCharacterThing.y += 70;
+			weekCharacterThing.x += 1200;
 			grpWeekCharacters.add(weekCharacterThing);
+
+			FlxTween.tween(weekCharacterThing, {x:0}, 2.4, {ease: FlxEase.expoInOut});
 		}
 
 		difficultySelectors = new FlxGroup();
