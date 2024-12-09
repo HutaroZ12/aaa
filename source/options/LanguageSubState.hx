43f7ece468f41031110b296ab5b0b23c9a652f2e
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-12-09 19:08:11 -0300

    Update LanguageSubState.hx

--- source/options/LanguageSubState.hx
+++ source/options/LanguageSubState.hx
@@ -133,7 +133,7 @@ class LanguageSubState extends MusicBeatSubstate
 			changedLanguage = true;
 		}
 		
-		if(controls.ACCEPT || 'pt-BR')
+		if(controls.ACCEPT || languages == 'pt-BR')
 		{
 		FlxG.sound.play(Paths.sound('Brasilsil'), 0.6);
 		}
