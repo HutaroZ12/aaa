6e0030a0bf37e434c8488dc603350aa92ffea51a
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-12-10 19:57:02 -0300

    Update LanguageSubState.hx

--- source/options/LanguageSubState.hx
+++ source/options/LanguageSubState.hx
@@ -134,13 +134,13 @@ class LanguageSubState extends MusicBeatSubstate
 		{
 		changedLanguage = true;	
 
-			FlxFlicker.flicker(languages[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
-	        {		 
 			FlxG.sound.play(Paths.sound('confirmMenu'));
 			ClientPrefs.data.language = languages[curSelected];
 			//trace(ClientPrefs.data.language);
 			ClientPrefs.saveSettings();
 			Language.reloadPhrases();
+			FlxFlicker.flicker(languages[curSelected], 1, 0.06, false, false, function(flick:FlxFlicker)
+			{
 			changedLanguage = true;
 		}
 	}
