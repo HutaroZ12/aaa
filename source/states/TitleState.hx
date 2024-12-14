f97a6ebede7e872b346476ff769084e2de181a29
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2024-12-13 22:03:28 -0300

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -416,14 +416,14 @@ class TitleState extends MusicBeatState
 			{
 				titleText.color = FlxColor.WHITE;
 				titleText.alpha = 1;
-
-				FlxTween.tween(FlxG.camera, {y: 2000}, 3.4, {ease: FlxEase.expoOut});
 				
 				if(titleText != null) titleText.animation.play('press');
 
 				FlxG.camera.flash(ClientPrefs.data.flashing ? FlxColor.WHITE : 0x4CFFFFFF, 1);
 				FlxG.sound.play(Paths.sound('confirmMenu'), 0.7);
 
+				FlxTween.tween(FlxG.camera, {y: 2000}, 2.4, {ease: FlxEase.expoOut});
+				
 				transitioning = true;
 				// FlxG.sound.music.stop();
 
