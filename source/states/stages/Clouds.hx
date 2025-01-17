46aaa75ad26c86bf75600d3d69c076e41110d0e0
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-17 16:26:32 -0300

    Update Clouds.hx

--- source/states/stages/Clouds.hx
+++ source/states/stages/Clouds.hx
@@ -4,15 +4,46 @@ import states.stages.objects.*;
 
 class Clounds extends BaseStage
 {
-	// If you're moving your stage from PlayState to a stage file,
-	// you might have to rename some variables if they're missing, for example: camZooming -> game.camZooming
-
+	var layer0:FlxSprite;
+	var layer1:FlxSprite;
+	var layer2:FlxSprite;
+	var layer3:FlxSprite;
+	var layer4:FlxSprite;
+	var layer5:FlxSprite;
+	var blackBars:FlxSprite;
+	
 	override function create()
 	{
-		// Spawn your stage sprites here.
-		// Characters are not ready yet on this function, so you can't add things above them yet.
-		// Use createPost() if that's what you want to do.
-	}
+	layer0 = new BGSprite('stages/sky/layer0', -500, -300);
+        layer0.scrollFactor.set(1, 1);
+        add(layer0);
+
+	layer1 = new BGSprite('stages/sky/layer1', -500, -300);
+        layer1.scrollFactor.set(1, 1);
+        add(layer1);
+
+	layer2 = new BGSprite('stages/sky/layer2', -500, -300);
+        layer2.scrollFactor.set(1, 1);
+        add(layer2);
+
+	layer3 = new BGSprite('stages/sky/layer3', -500, -300);
+        layer3.scrollFactor.set(1, 1);
+        add(layer3);
+
+	layer4 = new BGSprite('stages/sky/layer4', -500, -300);
+        layer4.scrollFactor.set(1, 1);
+        add(layer4);
+
+	layer5 = new BGSprite('stages/sky/layer5', -500, -300);
+        layer5.scrollFactor.set(1, 1);
+        add(layer5);
+
+	blackBars = new BGSprite('movieBars', 0, 0);
+        blackBars.scrollFactor.set(1, 1);
+        blackBars.scale.x = 1;
+        blackBars.scale.y = 1;
+        blackBars.camera = camHUD;
+        add(blackBars);
 	
 	override function createPost()
 	{
