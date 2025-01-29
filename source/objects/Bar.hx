facd12e0ba7aeaccbd88adbdf454a4fbf07553fb
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-28 22:33:20 -0300

    Update Bar.hx

--- source/objects/Bar.hx
+++ source/objects/Bar.hx
@@ -30,11 +30,11 @@ class Bar extends FlxSpriteGroup
 		barWidth = Std.int(bg.width - 6);
 		barHeight = Std.int(bg.height - 6);
 
-		leftBar = new FlxSprite().loadGraphic(Paths.image('healthBar'), FlxColor.WHITE);
-		leftBar.color = FlxColor.WHITE;
+		leftBar = new FlxSprite().makeGraphic(Std.int(bg.width), Std.int(bg.height), FlxColor.WHITE);
+		//leftBar.color = FlxColor.WHITE;
 		leftBar.antialiasing = antialiasing = ClientPrefs.data.antialiasing;
 
-		rightBar = new FlxSprite().loadGraphic(Paths.image('healthBar'), FlxColor.WHITE);
+		rightBar = new FlxSprite().makeGraphic(Std.int(bg.width), Std.int(bg.height), FlxColor.WHITE);
 		rightBar.color = FlxColor.BLACK;
 		rightBar.antialiasing = ClientPrefs.data.antialiasing;
 
