bddb9de5324f1043419d460f76aee3d854a69323
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-29 02:34:41 -0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -533,7 +533,6 @@ class PlayState extends MusicBeatState
 		healthBar = new Bar(0, FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : -600), 'healthBar', function() return health, 0, 2);
 		healthBar.screenCenter(X);
 		healthBar.y = 610;
-		healthBar.color = reloadHealthBarColors;
 		healthBar.leftToRight = false;
 		healthBar.scrollFactor.set();
 		healthBar.visible = !ClientPrefs.data.hideHud;
