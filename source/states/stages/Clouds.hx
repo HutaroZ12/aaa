401863e5d3879f7a72d3353dd9befed1e4eb1e22
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-30 04:04:21 -0300

    Update Clouds.hx

--- source/states/stages/Clouds.hx
+++ source/states/stages/Clouds.hx
@@ -26,7 +26,6 @@ class Clouds extends BaseStage
         layer1b = new BGSprite('stages/sky/layer1', layer1.x + layer1.width, -300); // segunda instância
         layer1b.scrollFactor.set(1, 1);
         add(layer1b);
-        moveLayerContinuous(layer1, layer1b, 20);
 
         // Camada 2
         layer2 = new BGSprite('stages/sky/layer2', -500, -300);
@@ -35,10 +34,13 @@ class Clouds extends BaseStage
         layer2b = new BGSprite('stages/sky/layer2', layer2.x + layer2.width, -300); // segunda instância
         layer2b.scrollFactor.set(0.85, 0.85);
         add(layer2b);
-        moveLayerContinuous(layer2, layer2b, 15);
 
         layer3 = new BGSprite('stages/sky/layer3', -500, -300, 1, 1);
         add(layer3);
+
+        // Inicia o movimento contínuo das camadas
+        moveLayerContinuous(layer1, layer1b, 20);
+        moveLayerContinuous(layer2, layer2b, 15);
     }
 
     override function createPost()
@@ -69,14 +71,12 @@ class Clouds extends BaseStage
 
         // Atualiza a posição das camadas continuamente
         layer.update = function(elapsed:Float):Void {
-            super.update(elapsed);
             if (layer.x + layer.width < 0) {
                 layer.x = layerb.x + layerb.width;
             }
         };
 
         layerb.update = function(elapsed:Float):Void {
-            super.update(elapsed);
             if (layerb.x + layerb.width < 0) {
                 layerb.x = layer.x + layer.width;
             }
