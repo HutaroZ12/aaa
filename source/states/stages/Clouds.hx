0820ddf67ae1b4d57fe4c6bc120e4acb10241667
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-30 11:56:24 -0300

    Update Clouds.hx

--- source/states/stages/Clouds.hx
+++ source/states/stages/Clouds.hx
@@ -7,12 +7,10 @@ class Clouds extends BaseStage
     var layer0:FlxSprite;
     var layer1:FlxSprite;
     var layer2:FlxSprite;
-    var layer1b:FlxSprite; // segunda instância da camada 1
-    var layer2b:FlxSprite; // segunda instância da camada 2
     var layer3:FlxSprite;
     var layer4:FlxSprite;
     var layer5:FlxSprite;
-
+ 
     override function create()
     {       
         layer0 = new BGSprite('stages/sky/layer0', -500, -300);
@@ -23,24 +21,14 @@ class Clouds extends BaseStage
         layer1 = new BGSprite('stages/sky/layer1', -500, -300);
         layer1.scrollFactor.set(1, 1);
         add(layer1);
-        layer1b = new BGSprite('stages/sky/layer1', layer1.x + layer1.width, -300); // segunda instância
-        layer1b.scrollFactor.set(1, 1);
-        add(layer1b);
 
         // Camada 2
         layer2 = new BGSprite('stages/sky/layer2', -500, -300);
         layer2.scrollFactor.set(0.85, 0.85);
         add(layer2);
-        layer2b = new BGSprite('stages/sky/layer2', layer2.x + layer2.width, -300); // segunda instância
-        layer2b.scrollFactor.set(0.85, 0.85);
-        add(layer2b);
 
         layer3 = new BGSprite('stages/sky/layer3', -500, -300, 1, 1);
         add(layer3);
-
-        // Inicia o movimento contínuo das camadas
-        moveLayerContinuous(layer1, layer1b, 20);
-        moveLayerContinuous(layer2, layer2b, 15);
     }
 
     override function createPost()
@@ -62,24 +50,4 @@ class Clouds extends BaseStage
                 boyfriend.playAnim('hey', true);
             case START:
         }
-
-    function moveLayerContinuous(layer:FlxSprite, layerb:FlxSprite, speed:Float)
-{
-    // Define a velocidade de movimento das camadas
-    layer.velocity.x = -speed;
-    layerb.velocity.x = -speed;
-
-    // Atualiza a posição das camadas continuamente
-    layer.update = function(elapsed:Float):Void {
-        if (layer.x + layer.width < 0) {
-            layer.x = layerb.x + layerb.width;
-        }
-    };
-
-    layerb.update = function(elapsed:Float):Void {
-        if (layerb.x + layerb.width < 0) {
-            layerb.x = layer.x + layer.width;
-        }
-    };
-  }
 }
