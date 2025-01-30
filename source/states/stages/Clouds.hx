60ee57b85582f0fdd8b40127fab5e3e3d6317b7c
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-30 03:55:58 -0300

    Update Clouds.hx

--- source/states/stages/Clouds.hx
+++ source/states/stages/Clouds.hx
@@ -7,8 +7,6 @@ class Clouds extends BaseStage
     var layer0:FlxSprite;
     var layer1:FlxSprite;
     var layer2:FlxSprite;
-    var layer1b:FlxSprite; // segunda instância da camada 1
-    var layer2b:FlxSprite; // segunda instância da camada 2
     var layer3:FlxSprite;
     var layer4:FlxSprite;
     var layer5:FlxSprite;
@@ -18,25 +16,16 @@ class Clouds extends BaseStage
         layer0 = new BGSprite('stages/sky/layer0', -500, -300);
         layer0.scrollFactor.set(1, 1);
         add(layer0);
-        moveLayer0();
 
         // Camada 1
         layer1 = new BGSprite('stages/sky/layer1', -500, -300);
         layer1.scrollFactor.set(1, 1);
         add(layer1);
-        layer1b = new BGSprite('stages/sky/layer1', layer1.x + layer1.width, -300); // segunda instância
-        layer1b.scrollFactor.set(1, 1);
-        add(layer1b);
-        moveLayerContinuous(layer1, layer1b, 20);
 
         // Camada 2
         layer2 = new BGSprite('stages/sky/layer2', -500, -300);
         layer2.scrollFactor.set(0.85, 0.85);
         add(layer2);
-        layer2b = new BGSprite('stages/sky/layer2', layer2.x + layer2.width, -300); // segunda instância
-        layer2b.scrollFactor.set(0.85, 0.85);
-        add(layer2b);
-        moveLayerContinuous(layer2, layer2b, 15);
 
         layer3 = new BGSprite('stages/sky/layer3', -500, -300, 1, 1);
         add(layer3);
@@ -61,38 +50,4 @@ class Clouds extends BaseStage
                 boyfriend.playAnim('hey', true);
             case START:
         }
-
-    function moveLayer0()
-    {
-        FlxTween.tween(layer0, {x: -1500}, 100, {ease: FlxEase.linear, onComplete: resetLayer0});
-    }
-
-    function resetLayer0(tween:FlxTween):Void
-    {
-        layer0.x = 0;
-        moveLayer0();
-    }
-
-    function moveLayerContinuous(layer:FlxSprite, layerb:FlxSprite, speed:Float)
-    {
-        // Define a velocidade de movimento das camadas
-        layer.velocity.x = -speed;
-        layerb.velocity.x = -speed;
-
-        // Atualiza a posição das camadas continuamente
-        layer.update = function(elapsed:Float):Void {
-            super.update(elapsed);
-            if (layer.x + layer.width < 0) {
-                layer.x = layerb.x + layerb.width;
-            }
-            if (layerb.x + layerb.width < 0) {
-                layerb.x = layer.x + layer.width;
-            }
-        };
-    }
-
-    function addShader(layer:FlxSprite)
-    {
-        // Adicione seu código de shader aqui, se necessário
-    }
 }
