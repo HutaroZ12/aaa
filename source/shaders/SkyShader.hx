73221efcf218958ed6d4d0643e433e0ff45ae0a0
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-30 01:21:05 -0300

    Create SkyShader.hx

---
+++ source/shaders/SkyShader.hx
@@ -0,0 +1,13 @@
+class SkyShader {
+    public static function loadShaders():ShaderProgram {
+        var vertexShader = Assets.loadText("assets/shaders/sky_vertex.glsl");
+        var fragmentShader = Assets.loadText("assets/shaders/sky_fragment.glsl");
+
+        var program = new ShaderProgram();
+        program.attach(vertexShader, GL.VERTEX_SHADER);
+        program.attach(fragmentShader, GL.FRAGMENT_SHADER);
+        program.link();
+
+        return program;
+    }
+}
