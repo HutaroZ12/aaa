20e52df0870475c9eea433dddbc7834799bdefef
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-01-31 20:56:51 -0300

    Update Clouds.hx

--- source/states/stages/Clouds.hx
+++ source/states/stages/Clouds.hx
@@ -56,12 +56,14 @@ class Clouds extends BaseStage
 
     override function stepHit()
     {
-    if (curSong == 'clouding')
-    {
-    if (curStep == 256) {
-    FlxG.camera.flash(FlxColor.WHITE, 2);
-    }
-                
+        if (curSong == 'clouding')
+        {
+            if (curStep == 256) {
+                FlxG.camera.flash(FlxColor.WHITE, 2);
+            }
+        }
+    } 
+
     override function createPost()
     {
         layer5 = new BGSprite('stages/sky/layer5', -500, -300);
@@ -87,27 +89,27 @@ class Clouds extends BaseStage
 
     function parallaxUpdate(elapsed:Float)
     {
-            // Atualizar a posição das camadas
-            layer1.x -= 50 * elapsed; // Ajuste a velocidade conforme necessário
-            layer1b.x -= 50 * elapsed; // Ajuste a velocidade conforme necessário
+        // Atualizar a posição das camadas
+        layer1.x -= 50 * elapsed; // Ajuste a velocidade conforme necessário
+        layer1b.x -= 50 * elapsed; // Ajuste a velocidade conforme necessário
 
-            layer2.x -= 8 * elapsed; // Ajuste a velocidade conforme necessário
-            layer2b.x -= 8 * elapsed; // Ajuste a velocidade conforme necessário
+        layer2.x -= 8 * elapsed; // Ajuste a velocidade conforme necessário
+        layer2b.x -= 8 * elapsed; // Ajuste a velocidade conforme necessário
 
-            // Reposicionar camadas para loop infinito
-            if (layer1.x + layer1.width <= 0){
-                layer1.x = layer1b.x + layer1b.width;
-            }
-            if (layer1b.x + layer1b.width <= 0){
-                layer1b.x = layer1.x + layer1.width;
-            }
+        // Reposicionar camadas para loop infinito
+        if (layer1.x + layer1.width <= 0){
+            layer1.x = layer1b.x + layer1b.width;
+        }
+        if (layer1b.x + layer1b.width <= 0){
+            layer1b.x = layer1.x + layer1.width;
+        }
 
-            if (layer2.x + layer2.width <= 0){
-                layer2.x = layer2b.x + layer2b.width;
-            }
-            if (layer2b.x + layer2b.width <= 0){
-                layer2b.x = layer2.x + layer2.width;
-            }
+        if (layer2.x + layer2.width <= 0){
+            layer2.x = layer2b.x + layer2.width;
+        }
+        if (layer2b.x + layer2.width <= 0){
+            layer2b.x = layer2.x + layer2.width;
+        }
     }
 
     override function countdownTick(count:Countdown, num:Int)
@@ -126,4 +128,4 @@ class Clouds extends BaseStage
                 }
         }
     }
-        }
+}
