c18221eb4eb909ffe7c075655bc9cc225c0aafac
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-02-28 16:15:30 -0300

    Update mobile.bat

--- setup/mobile.bat
+++ setup/mobile.bat
@@ -1,7 +1,6 @@
 @echo off
 color 0a
 cd ..
-@echo on
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
 haxelib git hxcpp https://github.com/mcagabe19-stuff/hxcpp --quiet
@@ -14,7 +13,7 @@ haxelib install hscript-iris 1.1.3 --quiet
 haxelib install tjson 1.4.0 --quiet
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 haxelib git linc_luajit https://github.com/MobilePorting/linc_luajit-0.7plus --quiet
-haxelib install hxdiscord_rpc 1.2.4 --quiet --skip-dependencies
+haxelib git hxdiscord_rpc https://github.com/MobilePorting/hxdiscord_rpc --quiet --skip-dependencies
 haxelib git hxvlc https://github.com/MobilePorting/hxvlc --quiet --skip-dependencies
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet --skip-dependencies
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
