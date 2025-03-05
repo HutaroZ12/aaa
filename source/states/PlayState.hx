012d2c1aea2c97d69c706c8ad5074ae62c1b0987
Hutaro <146780983+HutaroZ12@users.noreply.github.com>
2025-03-05 14:15:17 -0300

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -533,13 +533,13 @@ class PlayState extends MusicBeatState
 
 		healthBarBG = new FlxSprite(30, FlxG.height - 75).loadGraphic(Paths.image('healthBarBG'));
 		healthBarBG.screenCenter(X);
-		healthBarBG.y = 619;
+		healthBarBG.y = 617;
 		healthBarBG.scrollFactor.set();
 		healthBarBG.visible = !ClientPrefs.data.hideHud;
 		healthBarBG.alpha = ClientPrefs.data.healthBarAlpha;
 		uiGroup.add(healthBarBG);
 
-		if(ClientPrefs.data.downScroll) healthBarBG.y = 39;
+		if(ClientPrefs.data.downScroll) healthBarBG.y = 57;
 		
 		healthBar = new Bar(0, FlxG.height * (!ClientPrefs.data.downScroll ? 0.89 : -600), 'healthBar', function() return health, 0, 2);
 		healthBar.screenCenter(X);
@@ -550,7 +550,7 @@ class PlayState extends MusicBeatState
 		healthBar.alpha = ClientPrefs.data.healthBarAlpha;
 		uiGroup.add(healthBar);
 
-		if(ClientPrefs.data.downScroll) healthBar.y = 40;
+		if(ClientPrefs.data.downScroll) healthBar.y = 60;
 		reloadHealthBarColors();
 		
 		iconP1 = new HealthIcon(boyfriend.healthIcon, true);
